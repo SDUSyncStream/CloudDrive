239486e008381a9702f385c62aea6dfa15cbd55c
SiJuzheng <sijuzheng0831@gmail.com>
2025-07-07 10:08:37 +0800

    cicd fix

---
+++ packages/eslint-config-custom/index.js
@@ -0,0 +1,40 @@
+module.exports = {
+  extends: [
+    'eslint:recommended',
+    '@typescript-eslint/recommended',
+    'plugin:vue/vue3-recommended'
+  ],
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+    ecmaVersion: 2020,
+    sourceType: 'module'
+  },
+  plugins: [
+    '@typescript-eslint',
+    'vue'
+  ],
+  rules: {
+    // TypeScript rules
+    '@typescript-eslint/no-unused-vars': 'error',
+    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-empty-function': 'off',
+    
+    // Vue rules
+    'vue/multi-word-component-names': 'off',
+    'vue/no-unused-vars': 'error',
+    
+    // General rules
+    'no-console': 'warn',
+    'no-debugger': 'error',
+    'prefer-const': 'error',
+    'no-var': 'error'
+  },
+  env: {
+    node: true,
+    es6: true,
+    browser: true
+  }
+};
