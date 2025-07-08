3f599b0b5447ef43872d991c2352883fe88f85b9
csc <496484076@qq.com>
2025-07-08 10:09:27 +0800

    创建三个微服务

---
+++ apps/file_recycle_server/src/test/java/com/example/file_recycle_server/FileRecycleServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.file_recycle_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileRecycleServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
