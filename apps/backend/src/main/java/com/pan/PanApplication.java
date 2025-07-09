93ffb6c9b62648ba6c6c687d83a10869ccc55a2f
JuzhengSi <sijuzheng0831@gmail.com>
2025-07-09 10:10:15 +0800

    更新后端服务基础内容

---
+++ apps/backend/src/main/java/com/pan/PanApplication.java
@@ -0,0 +1,39 @@
+package com.pan;
+
+
+import com.pan.config.AppConfig;
+import com.pan.pojo.constants.Constants;
+import com.pan.spring.ApplicationContextProvider;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.DependsOn;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.servlet.MultipartConfigElement;
+
+
+@EnableAsync
+@SpringBootApplication(scanBasePackages = {"com.pan"})
+@MapperScan(basePackages = {"com.pan.mappers"})
+@EnableTransactionManagement
+@EnableScheduling
+public class PanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PanApplication.class, args);
+    }
+
+    @Bean
+    @DependsOn({"applicationContextProvider"})
+    MultipartConfigElement multipartConfigElement() {
+        AppConfig appConfig = (AppConfig) ApplicationContextProvider.getBean("appConfig");
+        MultipartConfigFactory factory = new MultipartConfigFactory();
+        factory.setLocation(appConfig.getProjectFolder() + Constants.FILE_FOLDER_TEMP);
+        return factory.createMultipartConfig();
+    }
+}
