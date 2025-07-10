ed810585ce153e18ead9280181ee6c057ae066e5
koala <126163334+xiongmingxia@users.noreply.github.com>
2025-07-11 01:40:16 +0800

    实现了创建分享链接

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -12,25 +12,30 @@ export default defineConfig({
   server: {
     port: 3000,
     proxy: {
-      '/api/showShare': {
-        target: 'http://localhost:8093',
-        changeOrigin: true,
-      },
+      
       '/admin-api': {
         target: 'http://localhost:8083',
+        changeOrigin: true,
+      },
+      '/api/showShare': {
+        target: 'http://localhost:8093',
         changeOrigin: true,
       },
       '/api': {
         target: 'http://localhost:8080',
         changeOrigin: true,
       },
-      '/fileup': {
-        target: 'http://localhost:8090',
-        changeOrigin: true,
-      },
       '/file': {
         target: 'http://localhost:8099',
         changeOrigin: true,
+      },
+      
+      
+      // 优化 /api/showShare 代理，匹配所有 /api/showShare 及其子路径
+      '/share/shareFile': {
+        target: 'http://localhost:8093/api',
+        changeOrigin: true,
+        rewrite: path => path.replace(/^\/share/, '/share'),
       },
       '/share/loadShareList': {
         target: 'http://localhost:8093/api',
@@ -42,6 +47,7 @@ export default defineConfig({
         changeOrigin: true,
         rewrite: path => path.replace(/^\/share/, '/share'),
       },
+      
     },
   },
   build: {
